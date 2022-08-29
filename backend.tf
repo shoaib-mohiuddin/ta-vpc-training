495d716044d0d742862e3c52a39a789f470e7449
shoaib-mohiuddin <shoaib.mohiuddin@cloudreach.com>
2022-08-29 19:15:29 +0530

    Modifies key/prefix for s3 bucket

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
         bucket = "talent-academy-shoaib-lab-tfstates"
-        key = "ta-vpc/vpc/terraform.tfstates"
+        key = "talent-academy/vpc/terraform.tfstates"
         region = "eu-west-1"
         dynamodb_table = "terraform-lock"
     }
