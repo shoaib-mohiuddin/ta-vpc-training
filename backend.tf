c4565384aabb1e5372ea04390ad57f588f5b3955
shoaib-mohiuddin <shoaib.mohiuddin@cloudreach.com>
2025-07-17 16:21:03 +0530

    replaces deprecated dynamodb state locking with s3 native state locking

--- backend.tf
+++ backend.tf
@@ -1,8 +1,9 @@
 terraform {
     backend "s3" {
-        bucket = "talent-academy-shoaib-lab-tfstates"
+        bucket = "talent-academy-lab-shoaib-tfstates-166916347510"
         key = "talent-academy/vpc/terraform.tfstates"
         region = "eu-west-1"
-        dynamodb_table = "terraform-lock"
+        # dynamodb_table = "terraform-lock" # Deprecated, use native state locking with S3
+        use_lockfile = true # S3 native state locking
     }
 }
